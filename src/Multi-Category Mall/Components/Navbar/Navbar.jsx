696f5d49677b01846c82cee48702dee833f07ddf
Ahmed Attia <ahmadyattia@gmail.com>
2026-04-02 12:52:15 +0200

    Added Navbar.jsx in the appropriate folder.

---
+++ src/Multi-Category Mall/Components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import styles from "/Users/ahmadattia/portfolio/src/Multi-Category Mall/Styles/Navbar.module.css";
+import NavbarCart from "../Cart/NavbarCart";
+import NavbarSettings from "./Settings/NavbarSettings";
+
+const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(null);
+
+  return (
+    <nav>
+      <div id={styles.navbar}>
+        <h1 id={styles.logo}>Shop Site</h1>
+        <div className={styles.mainNavigation}>
+          <Link to="/home">
+            <div className={styles.mainNavLnkBox}>
+              <p>Home</p>
+            </div>
+          </Link>
+          <Link to="/shop">
+            <div className={styles.mainNavLnkBox}>
+              <p>Shop</p>
+            </div>
+          </Link>
+          <Link to="/about">
+            <div className={styles.mainNavLnkBox}>
+              <p>About</p>
+            </div>
+          </Link>
+        </div>
+
+        <div className={styles.rightContainer}>
+          <NavbarCart openMenu={openMenu} setOpenMenu={setOpenMenu} />
+          <NavbarSettings openMenu={openMenu} setOpenMenu={setOpenMenu} />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
