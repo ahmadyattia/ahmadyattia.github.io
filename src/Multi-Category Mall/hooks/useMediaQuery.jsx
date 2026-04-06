d37633f178447524b2afe95d971c172d7cc47a6a
Ahmed Attia <ahmadyattia@gmail.com>
2026-04-06 21:58:31 +0200

    Implemented hook (used for listening to media query

--- src/Multi-Category Mall/hooks/useMediaQuery.jsx
+++ src/Multi-Category Mall/hooks/useMediaQuery.jsx
@@ -1,7 +1,26 @@
 import { useState, useEffect } from "react";
 
-const useMediaQuery = () => {
-  return <div></div>;
+// hook for listening to media query
+const useMediaQuery = (query) => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+
+    // set initial value
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+
+    // define listener
+    const listener = () => setMatches(media.matches);
+
+    media.addEventListener("change", listener);
+
+    return () => media.removeEventListener("change", listener);
+  }, [query, matches]);
+
+  return matches;
 };
 
 export default useMediaQuery;
