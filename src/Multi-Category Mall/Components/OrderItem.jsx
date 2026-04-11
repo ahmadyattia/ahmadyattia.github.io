053185e7caef3e09d51f5335fd814c3ad2fabe9e
Ahmed Attia <ahmadyattia@gmail.com>
2026-04-11 02:57:58 +0200

    Added discount display to the orders in the orders page

--- src/Multi-Category Mall/Components/OrderItem.jsx
+++ src/Multi-Category Mall/Components/OrderItem.jsx
@@ -4,12 +4,27 @@ import { Link } from "react-router-dom";
 
 const OrderItem = ({ item }) => {
   const itemLocation = `/shop/${item.category}/${item.id}/${item.slug}`;
+  let discount = null;
+
+  if (item.discountPercentage > 0) {
+    discount = (
+      item.price -
+      item.price * (item.discountPercentage / 100)
+    ).toFixed(2);
+  }
 
   return (
     <div className={styles.mainBox}>
       <img className={styles.img} src={item.img} alt="" />
       <div className={styles.title}>{item.title}</div>
-      <div className={styles.price}>${item.price}</div>
+      {discount ? (
+        <div className={styles.price}>
+          <span className={styles.oldPrice}>${item.price}</span>{" "}
+          <span className={styles.newPrice}>${discount}</span>
+        </div>
+      ) : (
+        <div className={styles.price}>${item.price}</div>
+      )}
       <div className={styles.quantity}>Qty: {item.quantity}</div>
       <div className={styles.viewLinkBox}>
         <Link to={itemLocation} className={styles.viewLink}>
