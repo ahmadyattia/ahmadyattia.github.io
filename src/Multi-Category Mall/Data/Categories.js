911149994a263cad05072cd5642a44d498eccfcd
Ahmed Attia <ahmadyattia@gmail.com>
2026-05-04 07:44:53 +0300

    update categories for homepage carousel

--- src/Multi-Category Mall/Data/Categories.js
+++ src/Multi-Category Mall/Data/Categories.js
@@ -1,13 +1,13 @@
 const categories = [
   { name: "All", slug: "all" },
   { name: "Clothes", slug: "clothes" },
-  // { name: "Electronics", slug: "electronics" },
+  { name: "Electronics", slug: "electronics" },
   { name: "Furniture", slug: "furniture" },
-  // { name: "Shoes", slug: "shoes" },
-  { name: "Groceries", slug: "groceries" },
-  { name: "Fragrances", slug: "fragrances" },
-  { name: "Beauty", slug: "beauty" },
-  // { name: "Miscellaneous", slug: "miscellaneous" },
+  { name: "Shoes", slug: "shoes" },
+  // { name: "Groceries", slug: "groceries" },
+  // { name: "Fragrances", slug: "fragrances" },
+  // { name: "Beauty", slug: "beauty" },
+  { name: "Miscellaneous", slug: "miscellaneous" },
 ];
 
 export default categories;
