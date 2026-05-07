e3ee65062cbc60484d275a047921f9be290a4ac7
Ahmed Attia <ahmadyattia@gmail.com>
2026-05-07 13:45:04 +0300

    improve hero section

--- src/components/Homepage/Hero/Hero.jsx
+++ src/components/Homepage/Hero/Hero.jsx
@@ -1,69 +1,25 @@
-import { useContext, useEffect, useState } from "react";
-import { ProductsContext } from "../../../context/ProductsContext";
-import HeroCard from "./HeroCard";
 import styles from "../../../Styles/Homepage/hero/Hero.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
-  const { data } = useContext(ProductsContext);
-  let [displayedProductIndex, setDisplayedProductIndex] = useState(0);
-  const [fourRandomItems, setFourRandomItems] = useState([]);
-  let products = null;
+  const navigate = useNavigate();
 
-  if (data) {
-    products = data;
-  } else {
-    console.log("Failed to fetch data.");
+  function handleShopNow() {
+    navigate(`/shop`);
   }
-
-  useEffect(() => {
-    // generate 4 random products from the products data
-    // that are not groceries
-    let selected = [];
-
-    while (products && selected.length < 4) {
-      const randomItem = products[Math.floor(Math.random() * products.length)];
-
-      const alreadyChosen = selected.some((item) => item.id === randomItem.id);
-
-      if (!alreadyChosen && randomItem.category !== "groceries") {
-        selected.push(randomItem);
-      }
-    }
-
-    setFourRandomItems([...selected]);
-  }, [products]);
-
-  function handleSwitch(order) {
-    if (fourRandomItems.length === 0) return;
-
-    setDisplayedProductIndex((prevIndex) => {
-      if (order === "next") {
-        return prevIndex === 3 ? 0 : prevIndex + 1;
-      } else {
-        return prevIndex === 0 ? 3 : prevIndex - 1;
-      }
-    });
-  }
-
-  // automatic switching
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-  //     handleSwitch("next");
-  //   }, 15000);
-
-  //   return () => {
-  //     clearInterval(timer);
-  //   };
-  // }, [fourRandomItems]);
 
   return (
     <div id={styles.hero}>
-      {fourRandomItems.length > 0 && (
-        <HeroCard
-          item={fourRandomItems[displayedProductIndex]}
-          switchFn={handleSwitch}
-        />
-      )}
+      <div className={styles.heroCard}>
+        <div className={styles.body}>
+          <h1 className={styles.title}>
+            From Home to Heart: Find What You’re Looking For.
+          </h1>
+          <button className={styles.shopNowBtn} onClick={handleShopNow}>
+            Shop now
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
