f5ed219a5f47ac53149e962879a229686331ee4c
Ahmed Attia <ahmadyattia@gmail.com>
2026-06-01 09:59:38 +0300

    add animation to the categories

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
 import styles from "../Styles/CategoryCard.module.css";
+import { useEffect, useState } from "react";
 
 const CategoryCard = ({ slug, name }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    // timer for the mobile browser to delay display so that the animation works
+    const timer = setTimeout(() => {
+      setIsMounted(true);
+    }, 20);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Link className={styles.card} to={`/shop/${slug}?page=1`}>
-      <h3 className={styles.name}>{name}</h3>
+      <h3 className={`${styles.name} ${isMounted ? styles.fadeIn : ""}`}>
+        {name}
+      </h3>
     </Link>
   );
 };
